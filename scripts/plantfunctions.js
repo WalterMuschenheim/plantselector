e2eec4b2f4c8ac74d6afa10dcfa0d2f2b551e84e
Walter Muschenheim <walter.muschenheim@gmail.com>
2020-10-24 13:03:35 -0400

    removes buttons from cards, makes modal launch from click anywhere on card

--- scripts/plantfunctions.js
+++ scripts/plantfunctions.js
@@ -17,11 +17,11 @@ function addCards(list, rows, tips) {
         if (rows == true) {
             cards += '<div class="col mb-4">';
         };
-        cards += '<div class="card"';
-        if (list[i].imageURL) {cards += '><img class="card-img-top" src="' + list[i].imageURL + '" alt="">';}
-        else {cards +='data-card-type="explainer">'};
+        cards += '<div class="card';
+        if (list[i].imageURL) {cards += ' plant-type" data-i="' + i + '"><img class="card-img-top" src="' + list[i].imageURL + '" alt="">';}
+        else {cards +=' explainer">'};
         cards += '<div class="card-body"> <h5 class="card-title">' +  list[i].name + '</h5> <p class="card-text">' + list[i].description + '</p>';
-        if (list[i].imageURL) {cards += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#plantModal" data-i="' + i + '">Learn More</button>'};
+        //if (list[i].imageURL) {cards += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#plantModal" data-i="' + i + '">Learn More</button>'};
         cards += '</div> <span class="keywords" style="display: none;">' + list[i].keywords + '</span> <span class="height" style="display: none;">' + list[i].height + '</span> <span class="light" style="display: none;">' + list[i].light + '</span> <span class="care" style="display: none;">' + list[i].care + '</span> </div>';
         if (rows == true) {
             cards += '</div>';
@@ -31,19 +31,19 @@ function addCards(list, rows, tips) {
     document.getElementById("plantlist-inner").innerHTML = cards += "</div>";
 };
 
-addCards(plants, true, explainers);
+addCards(plants, false, explainers);
 
 $('.carousel').carousel({interval: 99999999});
 
-$('.carousel-care-link').click(function() {
+$('.carousel-care-link').on('click', function() {
     $('.carousel').carousel(1);
 });
 
-$('.carousel-light-link').click(function() {
+$('.carousel-light-link').on('click', function() {
     $('.carousel').carousel(2);
 });
 
-$('.carousellinkhome').click(function() {
+$('.carousellinkhome').on('click', function() {
     $('.carousel').carousel(0);
 });
 
@@ -55,7 +55,29 @@ $('.popover-dismiss').popover({
     trigger: 'focus'
 });
 
-$('#plantModal').on('show.bs.modal', function (event) {
+$('.plant-type').on('click', function() {
+    var integer = $(this).data('i');
+    $('#plantModal').modal('toggle');
+    
+    //var button = $(event.relatedTarget);
+    //var integer = button.data('i');
+    var title = plants[integer].name;
+    var content = plants[integer].longDescription;
+    var image = plants[integer].imageURL;
+    var light = plants[integer].light;
+    var height = plants[integer].height;
+    var care = plants[integer].care;
+
+    $('#plantModal').find('.modal-title').html(title);
+    $('#plantModal').find('#modal-content-container').html('<p>' + content + '</p>');
+    $('#plantModal').find('.modal-popover.top a').attr('data-content', light);
+    $('#plantModal').find('.modal-popover.middle a').attr('data-content', height);
+    $('#plantModal').find('.modal-popover.bottom a').attr('data-content', care);
+    $('#plantModal').find('#modal-image-container').html('<img src="' + image + '" alt="' + title + '" class="img-fluid">');
+
+})
+
+/*$('#plantModal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget);
         var integer = button.data('i');
         var title = plants[integer].name;
@@ -71,7 +93,7 @@ $('#plantModal').on('show.bs.modal', function (event) {
         $('#plantModal').find('.modal-popover.middle a').attr('data-content', height);
         $('#plantModal').find('.modal-popover.bottom a').attr('data-content', care);
         $('#plantModal').find('#modal-image-container').html('<img src="' + image + '" alt="' + title + '" class="img-fluid">');
-    });
+    });*/
 
 function plantfilter() {
     var value = $("#plantsearch").val().toLowerCase();
